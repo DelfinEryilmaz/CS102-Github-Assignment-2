2b22b0c1c28d2bdd0dffda7ab97118b7c62a7710
a2436 <asal.behrouzi.2004@gmail.com>
2025-09-26 15:48:39 +0300

    Implementation of part , finding the minimum and maximum of the array + Some Indentation.

--- CS102_HW1_Assg2.java
+++ CS102_HW1_Assg2.java
@@ -1,9 +1,10 @@
-import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 
 public class CS102_HW1_Assg2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         int option;
         boolean done = false;
         Scanner in = new Scanner(System.in);
@@ -11,9 +12,12 @@ public class CS102_HW1_Assg2 {
         
         // Getting user input to determine array size
         System.out.print("Please enter an integer to determine the array size: ");
-        try {
+        try 
+        {
             arraySize = in.nextInt();   
-        } catch (Exception e) {
+        } 
+        catch (Exception e) 
+        {
             System.out.println("Invalid input. Please enter an integer.");
         }
         
@@ -25,7 +29,8 @@ public class CS102_HW1_Assg2 {
         System.out.println(Arrays.toString(arr));
 
         // 0 for exitting
-        while( !done ){
+        while( !done )
+        {
             System.out.println("-------MENU OPTIONS-------\n" +
                             "1 - find the maximum of the array \n" +
                             "2 - find the minimum of the array \n" +
@@ -36,34 +41,40 @@ public class CS102_HW1_Assg2 {
 
             option = in.nextInt();
             //exiting
-            if (option == 0){
+            if (option == 0)
+            {
                 System.out.println("Successfully exitted.");
                 done = true;
             }
 
             // finding the maximum of the array
-            else if (option == 1 ){
+            else if (option == 1 )
+            {
                 int maximum;
                 maximum = maximum(arr);
                 System.out.println("The maximum element of the array is: " + maximum);
             }
 
             // finding the minimum of the array
-            else if (option == 2 ){
+            else if (option == 2 )
+            {
                 int minimum;
                 minimum = minimum(arr);
                 System.out.println("The minimum element of the array is: " + minimum);
             }
 
             // finding the average of the array and the difference of each element
-            else if (option == 3 ){
+            else if (option == 3 )
+            {
                 double[] avgDifference;
                 avgDifference = differencesFromAverage(arr);
                 System.out.println("The difference array is : " + Arrays.toString(avgDifference));
             }
 
             // finding the sum of the elements in odd and even indexes
-            else if (option == 4 ){
+            else if (option == 4 )
+            {
+
                 int[] oddAndEvenIndexSums = oddAndEvenIndexedSumFinder(arr);
                 int sumOfOdd = oddAndEvenIndexSums[0];
                 int sumOfEven = oddAndEvenIndexSums[1];
@@ -71,7 +82,8 @@ public class CS102_HW1_Assg2 {
                 System.out.println("Sum of even indexes: " + sumOfEven);
             }
 
-            else {
+            else 
+            {
                 System.out.println("Invalid Option");
             }
         }
@@ -79,35 +91,50 @@ public class CS102_HW1_Assg2 {
         in.close();
     }
     
-    private static int[] difference(int[] arr) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'difference'");
+    private static int minimum(int[] arr)//Finding the minimum of the array 
+    {
+        int min = arr[0];
+            for (int i = 1; i < arr.length; i++) 
+        {
+            if (arr[i] < min) 
+        {
+            min = arr[i];
+        }
+    }
+        return min;
     }
 
-    private static int minimum(int[] arr) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'minimum'");
+    private static int maximum(int[] arr)//Finding the maximum of the array 
+    {
+        ;int max = arr[0];
+            for (int i = 1; i < arr.length; i++) 
+        {
+            if (arr[i] > max) 
+        {
+            max = arr[i];
+        }
     }
-
-    private static int maximum(int[] arr) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'maximum'");
+            return max;
     }
 
     // Filling array with random numbers
-    public static void fillArray(int[] arr) {
+    public static void fillArray(int[] arr) 
+    {
         Random random = new Random();
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length; i++) 
+        {
             arr[i] = random.nextInt(101);
         }
     }
 
     // Finding the odd and even numbered numbers in the array
-    public static int[] oddAndEvenIndexedSumFinder(int[] array){
+    public static int[] oddAndEvenIndexedSumFinder(int[] array)
+    {
         int[] returnThis = new int[2] ;
         int oddSum = 0;
         int evenSum = 0;
-        for (int i = 0; i < array.length; i++){
+        for (int i = 0; i < array.length; i++)
+        {
             if (i % 2 == 0 && array.length > 1)
             {
                 evenSum += array[i];
@@ -124,20 +151,21 @@ public class CS102_HW1_Assg2 {
 
 
     //Displaying the difference of the elements in an array
-   public static double[] differencesFromAverage(int[] arr) {
-
+   public static double[] differencesFromAverage(int[] arr) 
+   {
     //finding the avg, the default value set as 0
     double avg = 0;
     double [] avgDifference = new double[arr.length];
-    for (int num : arr) {
+    for (int num : arr) 
+    {
         avg += num;
     }
     avg = avg / arr.length;
 
-    for (int i = 0; i < arr.length; i++) {
+    for (int i = 0; i < arr.length; i++) 
+    {
         avgDifference[i] = arr[i] - avg; 
     }
-
     return avgDifference;
     }
 }
